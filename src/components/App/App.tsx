ac874aa243cff2d54463a892a995a166573b59ac
koki-develop <kou.pg.0131@gmail.com>
2022-01-04 10:53:54 +0900

    vite + react で動くようにした

---
+++ src/components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App: React.VFC = React.memo(() => {
+  return (
+    <div>
+      <h1>Application</h1>
+    </div>
+  );
+});
+
+App.displayName = 'App';
+
+export default App
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import * as path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
-})
+  build: {
+    rollupOptions: {
+      input: {
+        app: path.resolve(__dirname, 'app.html'),
+        popup: path.resolve(__dirname, 'popup.html'),
+      },
+    },
+  },
+  plugins: [react()],
+});
