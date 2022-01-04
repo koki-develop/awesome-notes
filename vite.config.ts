f5fbdfacc55b9b6bfa81504b02d2d065642ff742
koki-develop <kou.pg.0131@gmail.com>
2022-01-04 11:04:44 +0900

    Revert "app.html, popup.html を src/ ディレクトリに移動"
    This reverts commit 1f546b77013b6143f757c11e0a98c0a38090ac6c.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
-import * as path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import * as path from 'path';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        app: path.resolve(__dirname, "src/app.html"),
-        popup: path.resolve(__dirname, "src/popup.html"),
+        app: path.resolve(__dirname, 'app.html'),
+        popup: path.resolve(__dirname, 'popup.html'),
       },
     },
   },
