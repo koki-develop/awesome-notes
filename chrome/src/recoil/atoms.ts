5fb7815aaaa80a88186191f5c6bb8919429724e9
koki-develop <kou.pg.0131@gmail.com>
2022-01-04 19:11:27 +0900

    selectedNoteIdState を作成

---
+++ chrome/src/recoil/atoms.ts
@@ -0,0 +1,7 @@
+import { atom } from 'recoil';
+import { LocalStorage } from '../lib/localStorage';
+
+export const selectedNoteIdState = atom<number | null>({
+  key: 'selectedNoteIdState',
+  default: LocalStorage.getSelectedNoteId(),
+});
