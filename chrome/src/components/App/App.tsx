025077c66e653fa6fb153a253e0f53292a1fe185
koki-develop <kou.pg.0131@gmail.com>
2022-01-05 14:29:03 +0900

    Layout コンポーネントを作成

--- chrome/src/components/App/App.tsx
+++ chrome/src/components/App/App.tsx
@@ -1,88 +1,16 @@
-import React, { useCallback, useState } from 'react';
+import React from 'react';
 import { RecoilRoot } from 'recoil';
-import { styled } from '@mui/material/styles';
-import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
-import CssBaseline from '@mui/material/CssBaseline';
-import IconButton from '@mui/material/IconButton';
-import Toolbar from '@mui/material/Toolbar';
-import MenuIcon from '@mui/icons-material/Menu';
-import NoteListDrawer from '../model/note/NoteListDrawer';
+import Layout from '../Layout';
 import NoteEditor from '../model/note/NoteEditor';
 
-const drawerWidth = 240;
-
-type AppBarProps = MuiAppBarProps & {
-  open: boolean;
-};
-
-const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: prop => prop !== 'open',
-})<AppBarProps>(({ theme, open }) => ({
-  transition: theme.transitions.create(['margin', 'width'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
-type MainProps = { open: boolean };
-
-const Main = styled('main', {
-  shouldForwardProp: prop => prop !== 'open',
-})<MainProps>(({ theme, open }) => ({
-  padding: theme.spacing(1),
-  transition: theme.transitions.create(['margin', 'width'], {
-    easing: theme.transitions.easing.sharp,
-    duration: theme.transitions.duration.leavingScreen,
-  }),
-  ...(open && {
-    width: `calc(100% - ${drawerWidth}px)`,
-    marginLeft: `${drawerWidth}px`,
-    transition: theme.transitions.create(['margin', 'width'], {
-      easing: theme.transitions.easing.easeOut,
-      duration: theme.transitions.duration.enteringScreen,
-    }),
-  }),
-}));
-
 const App: React.VFC = React.memo(() => {
-  const [openDrawer, setOpenDrawer] = useState<boolean>(false);
-
-  const handleClickMenu = useCallback(() => {
-    setOpenDrawer(true);
-  }, []);
-
-  const handleCloseDrawer = useCallback(() => {
-    setOpenDrawer(false);
-  }, []);
-
   return (
     <RecoilRoot>
-      <CssBaseline />
-      <AppBar position='static' open={openDrawer}>
-        <Toolbar>
-          <IconButton onClick={handleClickMenu}>
-            <MenuIcon />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
-      <NoteListDrawer
-        open={openDrawer}
-        width={drawerWidth}
-        onClose={handleCloseDrawer}
-      />
-      <Main open={openDrawer}>
+      <Layout>
         <div>
           <NoteEditor />
         </div>
-      </Main>
+      </Layout>
     </RecoilRoot>
   );
 });
---
+++ chrome/src/components/Layout/Layout.tsx
@@ -0,0 +1,107 @@
+import React, { useCallback, useState } from 'react';
+import { styled } from '@mui/material/styles';
+import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import IconButton from '@mui/material/IconButton';
+import Toolbar from '@mui/material/Toolbar';
+import MenuIcon from '@mui/icons-material/Menu';
+import NoteListDrawer from '../model/note/NoteListDrawer';
+
+const drawerWidth = 240;
+
+type AppBarProps = MuiAppBarProps & {
+  open: boolean;
+};
+
+const AppBar = styled(MuiAppBar, {
+  shouldForwardProp: prop => prop !== 'open',
+})<AppBarProps>(({ theme, open }) => ({
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+type MainProps = { open: boolean };
+
+const Main = styled('main', {
+  shouldForwardProp: prop => prop !== 'open',
+})<MainProps>(({ theme, open }) => ({
+  padding: theme.spacing(1),
+  transition: theme.transitions.create(['margin', 'width'], {
+    easing: theme.transitions.easing.sharp,
+    duration: theme.transitions.duration.leavingScreen,
+  }),
+  ...(open && {
+    width: `calc(100% - ${drawerWidth}px)`,
+    marginLeft: `${drawerWidth}px`,
+    transition: theme.transitions.create(['margin', 'width'], {
+      easing: theme.transitions.easing.easeOut,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+  }),
+}));
+
+export type LayoutProps = {
+  children: React.ReactNode;
+};
+
+const Layout: React.VFC<LayoutProps> = React.memo(props => {
+  return (
+    <>
+      <CssBaseline />
+      <LayoutContent {...props} />
+    </>
+  );
+});
+
+Layout.displayName = 'Layout';
+
+export default Layout;
+
+const LayoutContent: React.VFC<LayoutProps> = React.memo(props => {
+  const { children } = props;
+  const [openDrawer, setOpenDrawer] = useState<boolean>(false);
+
+  const handleClickMenu = useCallback(() => {
+    setOpenDrawer(true);
+  }, []);
+
+  const handleCloseDrawer = useCallback(() => {
+    setOpenDrawer(false);
+  }, []);
+
+  return (
+    <Box>
+      {/* header */}
+      <AppBar position='static' open={openDrawer}>
+        <Toolbar>
+          <IconButton onClick={handleClickMenu}>
+            <MenuIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
+      {/* drawer */}
+      <NoteListDrawer
+        open={openDrawer}
+        width={drawerWidth}
+        onClose={handleCloseDrawer}
+      />
+
+      {/* main content */}
+      <Main open={openDrawer}>{children}</Main>
+    </Box>
+  );
+});
+
+LayoutContent.displayName = 'LayoutContent';
