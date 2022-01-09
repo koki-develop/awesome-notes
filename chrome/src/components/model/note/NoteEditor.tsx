eca8ba4ac331634d2f0c7103f0c2e9b5bda56440
koki-develop <kou.pg.0131@gmail.com>
2022-01-09 22:59:29 +0900

    title と body は一緒に管理するようにした

--- chrome/src/components/model/note/NoteEditor.tsx
+++ chrome/src/components/model/note/NoteEditor.tsx
@@ -11,16 +11,17 @@ export type NoteEditorProps = {
 
 const NoteEditor: React.VFC<NoteEditorProps> = React.memo(props => {
   const { note } = props;
-  const [title, setTitle] = useState<string>(note.title);
-  const [body, setBody] = useState<string>(note.body);
+  const [content, setContent] = useState<Pick<Note, 'title' | 'body'>>({
+    title: note.title,
+    body: note.body,
+  });
 
   const editor = useEditor({
     extensions: [StarterKit],
-    content: body,
+    content: content.body,
     onUpdate({ editor }) {
       const title = editor.getText().trim().split('\n')[0].slice(0, 50);
-      setTitle(title);
-      setBody(editor.getHTML());
+      setContent({ title, body: editor.getHTML() });
     },
   });
 
@@ -29,25 +30,17 @@ const NoteEditor: React.VFC<NoteEditorProps> = React.memo(props => {
   // ノートを選択時
   useEffect(() => {
     if (!editor) return;
-    if (editor.getHTML() === note.body) return;
-    setBody(note.body);
+    setContent({ title: note.title, body: note.body });
     editor.chain().setContent(note.body).focus().run();
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [note.id]);
 
-  // タイトル更新時
-  useEffect(() => {
-    if (note.title === title) return;
-    updateNote(note.id, { title });
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [title, updateNote]);
-
   // 内容更新時
   useEffect(() => {
-    if (note.body === body) return;
-    updateNote(note.id, { body });
+    if (note.title === content.title && note.body === content.body) return;
+    updateNote(note.id, content);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [body, updateNote]);
+  }, [content, updateNote]);
 
   return <EditorContent className='note-editor' editor={editor} />;
 });
