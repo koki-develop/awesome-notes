681e419073abbd923d41cdb9ef442c2139f1ee04
koki-develop <kou.pg.0131@gmail.com>
2022-01-09 17:22:38 +0900

    alias の設定を追加

--- chrome/vite.config.ts
+++ chrome/vite.config.ts
@@ -3,6 +3,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   build: {
     rollupOptions: {
       input: {
