e0e17e79be88605a0952bb31aa807faef0ffb7b4
koki-develop <kou.pg.0131@gmail.com>
2022-01-12 10:57:27 +0900

    不要な import を削除

--- chrome/src/components/Layout/Layout.tsx
+++ chrome/src/components/Layout/Layout.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useMemo, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { useTheme } from '@mui/material/styles';
 import { useMediaQuery } from '@mui/material';
 import AppBar from '@mui/material/AppBar';
