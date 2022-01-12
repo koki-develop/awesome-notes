caf1bb43ab17b8340e3190350b5a240e8dcb16ff
koki-develop <kou.pg.0131@gmail.com>
2022-01-12 12:01:02 +0900

    ノート未選択時の表示を作成

--- chrome/src/components/App/App.tsx
+++ chrome/src/components/App/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { RecoilRoot } from 'recoil';
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
 import { useSelectedNote } from '@/hooks/noteHooks';
 import Layout from '@/components/Layout';
 import NoteEditor from '@/components/model/note/NoteEditor';
@@ -25,7 +27,33 @@ const AppContent: React.VFC<AppProps> = React.memo(props => {
 
   const note = useSelectedNote();
 
-  return <Layout popup={popup}>{note && <NoteEditor note={note} />}</Layout>;
+  return (
+    <Layout popup={popup}>
+      {note ? (
+        <NoteEditor note={note} />
+      ) : (
+        <Box
+          sx={{
+            alignItems: 'center',
+            backgroundColor: 'divider',
+            display: 'flex',
+            height: '100%',
+            justifyContent: 'center',
+          }}
+        >
+          <Typography
+            variant='h5'
+            sx={{
+              color: theme => theme.palette.text.disabled,
+              textAlign: 'center',
+            }}
+          >
+            ノートを選択してください
+          </Typography>
+        </Box>
+      )}
+    </Layout>
+  );
 });
 
 AppContent.displayName = 'AppContent';
