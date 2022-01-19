7b48ac987bc939b557e7660808d92af472d3430f
koki-develop <kou.pg.0131@gmail.com>
2022-01-19 16:20:22 +0900

    バブルメニューの見た目を最低限装飾

--- chrome/src/components/model/note/NoteEditor.tsx
+++ chrome/src/components/model/note/NoteEditor.tsx
@@ -7,6 +7,13 @@ import StarterKit from '@tiptap/starter-kit';
 import TaskList from '@tiptap/extension-task-list';
 import TaskItem from '@tiptap/extension-task-item';
 import Underline from '@tiptap/extension-underline';
+import IconButton from '@mui/material/IconButton';
+import Paper from '@mui/material/Paper';
+import BoldIcon from '@mui/icons-material/FormatBold';
+import CodeIcon from '@mui/icons-material/Code';
+import ItalicIcon from '@mui/icons-material/FormatItalic';
+import StrikethroughIcon from '@mui/icons-material/StrikethroughS';
+import UnderlineIcon from '@mui/icons-material/FormatUnderlined';
 import { useUpdateNote } from '@/hooks/noteHooks';
 import { Note } from '@/models/note';
 import NoteEditorTrailingNodeExtension from './NoteEditorTrailingNodeExtension';
@@ -73,6 +80,11 @@ const NoteEditor: React.VFC<NoteEditorProps> = React.memo(props => {
     editor.chain().focus().toggleUnderline().run();
   }, [editor]);
 
+  const handleClickStrikethrough = useCallback(() => {
+    if (!editor) return;
+    editor.chain().focus().toggleStrike().run();
+  }, [editor]);
+
   const handleClickCode = useCallback(() => {
     if (!editor) return;
     editor.chain().focus().toggleCode().run();
@@ -109,11 +121,90 @@ const NoteEditor: React.VFC<NoteEditorProps> = React.memo(props => {
   return (
     <>
       {editor && (
+        // TODO: リファクタ
         <BubbleMenu editor={editor}>
-          <button onClick={handleClickBold}>bold</button>
-          <button onClick={handleClickItalic}>italic</button>
-          <button onClick={handleClickUnderline}>underline</button>
-          <button onClick={handleClickCode}>code</button>
+          <Paper sx={{ p: 1 }}>
+            <IconButton
+              disableRipple
+              onClick={handleClickBold}
+              size='small'
+              sx={{
+                backgroundColor: editor.isActive('bold')
+                  ? 'divider'
+                  : undefined,
+                borderRadius: 0,
+                '&:not(:last-child)': {
+                  mr: 1,
+                },
+              }}
+            >
+              <BoldIcon />
+            </IconButton>
+            <IconButton
+              disableRipple
+              onClick={handleClickItalic}
+              size='small'
+              sx={{
+                backgroundColor: editor.isActive('italic')
+                  ? 'divider'
+                  : undefined,
+                borderRadius: 0,
+                '&:not(:last-child)': {
+                  mr: 1,
+                },
+              }}
+            >
+              <ItalicIcon />
+            </IconButton>
+            <IconButton
+              disableRipple
+              onClick={handleClickUnderline}
+              size='small'
+              sx={{
+                backgroundColor: editor.isActive('underline')
+                  ? 'divider'
+                  : undefined,
+                borderRadius: 0,
+                '&:not(:last-child)': {
+                  mr: 1,
+                },
+              }}
+            >
+              <UnderlineIcon />
+            </IconButton>
+            <IconButton
+              disableRipple
+              onClick={handleClickStrikethrough}
+              size='small'
+              sx={{
+                backgroundColor: editor.isActive('strike')
+                  ? 'divider'
+                  : undefined,
+                borderRadius: 0,
+                '&:not(:last-child)': {
+                  mr: 1,
+                },
+              }}
+            >
+              <StrikethroughIcon />
+            </IconButton>
+            <IconButton
+              disableRipple
+              onClick={handleClickCode}
+              size='small'
+              sx={{
+                backgroundColor: editor.isActive('code')
+                  ? 'divider'
+                  : undefined,
+                borderRadius: 0,
+                '&:not(:last-child)': {
+                  mr: 1,
+                },
+              }}
+            >
+              <CodeIcon />
+            </IconButton>
+          </Paper>
         </BubbleMenu>
       )}
       <EditorContent className='note-editor' editor={editor} />
