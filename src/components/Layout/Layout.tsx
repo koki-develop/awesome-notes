4bb14c5f6d039cfd76d053e6f76cbf7f68a973a3
koki-develop <kou.pg.0131@gmail.com>
2022-02-24 08:12:42 +0900

    ヘッダを LayoutHeader コンポーネントとして切り出した

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,17 +1,11 @@
-import MenuIcon from '@mui/icons-material/Menu';
 import { useMediaQuery } from '@mui/material';
-import AppBar from '@mui/material/AppBar';
-import Avatar from '@mui/material/Avatar';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
-import IconButton from '@mui/material/IconButton';
 import { useTheme, ThemeProvider } from '@mui/material/styles';
-import Toolbar from '@mui/material/Toolbar';
 import React, { useCallback, useEffect, useState } from 'react';
 import NoteListDrawer from '@/components/model/note/NoteListDrawer';
 import { LocalStorage } from '@/lib/localStorage';
-import GitHubIcon from '@/static/images/github-icon.svg';
+import LayoutHeader from './LayoutHeader';
 import { theme } from './theme';
 
 export type LayoutProps = {
@@ -44,15 +38,6 @@ const LayoutContent: React.VFC<LayoutProps> = React.memo(props => {
     LocalStorage.getOpenDrawer({ popup }),
   );
 
-  const handleClickOpenApp = useCallback(async () => {
-    const url = chrome.runtime.getURL('app.html');
-    chrome.tabs.create({ url });
-  }, []);
-
-  const handleClickMenu = useCallback(() => {
-    setOpenDrawer(true);
-  }, []);
-
   const handleCloseDrawer = useCallback(() => {
     setOpenDrawer(false);
   }, []);
@@ -65,54 +50,7 @@ const LayoutContent: React.VFC<LayoutProps> = React.memo(props => {
     <Box sx={{ height: '100vh', minWidth: 750, minHeight: 500 }}>
       <CssBaseline />
 
-      {/* header */}
-      <AppBar
-        position='static'
-        sx={theme => ({
-          justifyContent: 'center',
-          height: headerHeight,
-          transition: theme.transitions.create(['margin', 'width'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-          }),
-          ...(openDrawer && {
-            width: `calc(100% - ${drawerWidth}px)`,
-            marginLeft: `${drawerWidth}px`,
-            transition: theme.transitions.create(['margin', 'width'], {
-              easing: theme.transitions.easing.easeOut,
-              duration: theme.transitions.duration.enteringScreen,
-            }),
-          }),
-        })}
-      >
-        <Toolbar>
-          <Box sx={{ flexGrow: 1 }}>
-            {!openDrawer && (
-              <IconButton color='secondary' onClick={handleClickMenu}>
-                <MenuIcon />
-              </IconButton>
-            )}
-          </Box>
-          {popup && (
-            <Button
-              color='secondary'
-              variant='contained'
-              onClick={handleClickOpenApp}
-              sx={{ fontWeight: 'bold', mr: 1 }}
-            >
-              Open App
-            </Button>
-          )}
-          <IconButton
-            size='small'
-            href='https://github.com/koki-develop/awesome-notes'
-            target='_blank'
-            rel='noreferrer noopener'
-          >
-            <Avatar src={GitHubIcon} sx={{ height: 32, width: 32 }} />
-          </IconButton>
-        </Toolbar>
-      </AppBar>
+      <LayoutHeader popup={popup} />
 
       {/* drawer */}
       <NoteListDrawer
---
+++ src/components/Layout/LayoutHeader.tsx
@@ -0,0 +1,93 @@
+import MenuIcon from '@mui/icons-material/Menu';
+import { useMediaQuery } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import Avatar from '@mui/material/Avatar';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import { useTheme } from '@mui/material/styles';
+import Toolbar from '@mui/material/Toolbar';
+import React, { useCallback, useState } from 'react';
+import { LocalStorage } from '@/lib/localStorage';
+import GitHubIcon from '@/static/images/github-icon.svg';
+
+export type LayoutHeaderProps = {
+  popup?: boolean;
+};
+
+const LayoutHeader: React.VFC<LayoutHeaderProps> = React.memo(props => {
+  const { popup } = props;
+
+  const theme = useTheme();
+  const isSmDown = useMediaQuery(theme.breakpoints.down('sm'));
+
+  const headerHeight = 64;
+  const drawerWidth = isSmDown || popup ? 200 : 240;
+
+  const [openDrawer, setOpenDrawer] = useState<boolean>(
+    LocalStorage.getOpenDrawer({ popup }),
+  );
+
+  const handleClickOpenApp = useCallback(async () => {
+    const url = chrome.runtime.getURL('app.html');
+    chrome.tabs.create({ url });
+  }, []);
+
+  const handleClickMenu = useCallback(() => {
+    setOpenDrawer(true);
+  }, []);
+
+  return (
+    <AppBar
+      position='static'
+      sx={theme => ({
+        justifyContent: 'center',
+        height: headerHeight,
+        transition: theme.transitions.create(['margin', 'width'], {
+          easing: theme.transitions.easing.sharp,
+          duration: theme.transitions.duration.leavingScreen,
+        }),
+        ...(openDrawer && {
+          width: `calc(100% - ${drawerWidth}px)`,
+          marginLeft: `${drawerWidth}px`,
+          transition: theme.transitions.create(['margin', 'width'], {
+            easing: theme.transitions.easing.easeOut,
+            duration: theme.transitions.duration.enteringScreen,
+          }),
+        }),
+      })}
+    >
+      <Toolbar>
+        <Box sx={{ flexGrow: 1 }}>
+          {!openDrawer && (
+            <IconButton color='secondary' onClick={handleClickMenu}>
+              <MenuIcon />
+            </IconButton>
+          )}
+        </Box>
+        {popup && (
+          <Button
+            color='secondary'
+            variant='contained'
+            onClick={handleClickOpenApp}
+            sx={{ fontWeight: 'bold', mr: 1 }}
+          >
+            Open App
+          </Button>
+        )}
+        <IconButton
+          size='small'
+          href='https://github.com/koki-develop/awesome-notes'
+          target='_blank'
+          rel='noreferrer noopener'
+        >
+          <Avatar src={GitHubIcon} sx={{ height: 32, width: 32 }} />
+        </IconButton>
+      </Toolbar>
+    </AppBar>
+  );
+});
+
+LayoutHeader.displayName = 'LayoutHeader';
+
+export default LayoutHeader;
