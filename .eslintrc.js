dce79f7629bbb796962210bde9923a0dfc6ea77d
koki-develop <kou.pg.0131@gmail.com>
2022-02-23 22:27:12 +0900

    extends に prettier を追加

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
