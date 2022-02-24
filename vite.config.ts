8c80e6daedcd02637fc2b0edc1fa840802567c45
koki-develop <kou.pg.0131@gmail.com>
2022-02-24 09:27:35 +0900

    開発中は title を変えるようにした

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
 
+const title = process.env.NODE_ENV === 'production' ? 'Awesome Notes' : 'Awesome Notes (development)';
+
 export default defineConfig({
   resolve: {
     alias: {
@@ -18,6 +20,11 @@ export default defineConfig({
           entry: 'src/app.tsx',
           template: 'src/templates/app.html',
           filename: 'app.html',
+          injectOptions: {
+            data: {
+              title,
+            }
+          }
         },
         {
           entry: 'src/popup.tsx',
