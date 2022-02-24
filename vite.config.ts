50ca8a857dd60c2a271605fd05c9771653a3ad00
koki-develop <kou.pg.0131@gmail.com>
2022-02-24 09:17:54 +0900

    createHtmlPlugin を使うようにした

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import * as path from 'path';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
-import { minifyHtml } from 'vite-plugin-html';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
 export default defineConfig({
   resolve: {
@@ -9,13 +9,22 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  build: {
-    rollupOptions: {
-      input: {
-        app: path.resolve(__dirname, 'app.html'),
-        popup: path.resolve(__dirname, 'popup.html'),
-      },
-    },
-  },
-  plugins: [react(), minifyHtml()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      minify: true,
+      pages: [
+        {
+          entry: 'src/app.tsx',
+          filename: 'app.html',
+          template: 'app.html',
+        },
+        {
+          entry: 'src/popup.tsx',
+          filename: 'popup.html',
+          template: 'popup.html',
+        },
+      ],
+    }),
+  ],
 });
